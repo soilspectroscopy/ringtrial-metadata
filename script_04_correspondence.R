7572f466ecd6dda6a4c76784338ac07fb9c4aa4a
José Lucas Safanelli <zecojls@gmail.com>
2023-02-27 17:23:47 -0500

    correspondence analysis example

--- script_04_correspondence.R
+++ script_04_correspondence.R
@@ -1,9 +1,11 @@
 
 ## Loading packages
 library("tidyverse")
-library("FactoMineR")
+library("ggrepel")
 library("qs")
-library("corrplot")
+library("ca")
+
+options(scipen = 999)
 
 ## Folders
 mnt.dir <- "~/projects/mnt-ringtrial/"
@@ -13,16 +15,6 @@ dir.output <- "outputs/"
 ## Files
 scores <- qread(paste0(dir.metadata, "pca_clustering.qs"))
 scores
-
-## Example
-data(children)
-children
-res.ca <- CA (children, row.sup = 15:18, col.sup = 6:8)
-plot(res.ca)
-summary(res.ca)
-ellipseCA(res.ca)
-ellipseCA(res.ca,ellipse="col",col.col.ell=c(rep("blue",2),rep("transparent",3)),
-          invisible=c("row.sup", "col.sup"))
 
 ## Correspondence analysis
 metadata <- read_csv(paste0(dir.output, "instruments_metadata_clean.csv"))
@@ -45,49 +37,97 @@ ct.manufacturer
 chi.sq.result <- chisq.test(ct.manufacturer)
 chi.sq.result
 
-# at least 5 for any cell
+# observed
+chi.sq.result$observed
+
+# expected
 chi.sq.result$expected
 
-# residuals
+# this test must be run only with at least 5 values for any cell in the expected proportion
+round(chi.sq.result$expected, 0)
+
+# Pearson residuals - (observed - expected) / sqrt(expected)
 chi.sq.result$residuals
-corrplot(chi.sq.result$residuals, is.cor = FALSE)
-
-# contribution in percentage
-contrib <- 100*chi.sq.result$residuals^2/chi.sq.result$statistic
-round(contrib, 3)
-corrplot(contrib, is.cor = FALSE)
-
-ca.manufacturer <- CA(ct.manufacturer, graph = FALSE)
-summary(ca.manufacturer)
-
-plot(ca.manufacturer)
-ellipseCA(ca.manufacturer)
-
-ca.manufacturer$call
-ca.manufacturer$row
-ca.manufacturer$svd
 
 
-# model
-names(ca.data)
+# ca implementation
+ca.manufacturer <- ca(ct.manufacturer)
 
-ct.model <- table(ca.data$cluster, ca.data$model)
-ct.model
+# symmetric plot
+plot(ca(ct.manufacturer))
 
-chi.sq.result <- chisq.test(ct.model)
-chi.sq.result
-
-chi.sq.result$expected
-
-ca.data %>%
-  distinct(model) %>%
-  arrange(model)
-
-rownames(ct.model) <- c("C1", "C2", "C3", "C4")
-ct.model <- as.data.frame.matrix(ct.model)
-
-ca.model <- CA(ct.model, graph = FALSE)
-
-plot(ca.model)
+# asymmetric plot - row principal
+plot(ca(ct.manufacturer), map = "rowprincipal")
 
 
+# custom implementation
+ct.manufacturer.P    <- ct.manufacturer/sum(ct.manufacturer)
+ct.manufacturer.r    <- apply(ct.manufacturer.P, 1, sum)
+ct.manufacturer.c    <- apply(ct.manufacturer.P, 2, sum)
+ct.manufacturer.Dr   <- diag(ct.manufacturer.r)
+ct.manufacturer.Dc   <- diag(ct.manufacturer.c)
+ct.manufacturer.Drmh <- diag(1/sqrt(ct.manufacturer.r))
+ct.manufacturer.Dcmh <- diag(1/sqrt(ct.manufacturer.c))
+
+ct.manufacturer.P   <- as.matrix(ct.manufacturer.P)
+ct.manufacturer.S   <- ct.manufacturer.Drmh%*%(ct.manufacturer.P-ct.manufacturer.r%o%ct.manufacturer.c)%*%ct.manufacturer.Dcmh
+
+ct.manufacturer.svd <- svd(ct.manufacturer.S)
+ct.manufacturer.svd$d^2 # variance
+ct.manufacturer.svd$d^2/sum(ct.manufacturer.svd$d^2)*100 # cumulative variance
+
+ct.manufacturer.rsc <- ct.manufacturer.Drmh%*%ct.manufacturer.svd$u
+ct.manufacturer.csc <- ct.manufacturer.Dcmh%*%ct.manufacturer.svd$v
+ct.manufacturer.rpc <- ct.manufacturer.rsc%*%diag(ct.manufacturer.svd$d)
+ct.manufacturer.cpc <- ct.manufacturer.csc%*%diag(ct.manufacturer.svd$d)
+
+# standard coordinates
+row.standard.data <- tibble(type = "cluster",
+                            label = rownames(ct.manufacturer),
+                            PC1 = ct.manufacturer.rsc[,1],
+                            PC2 = ct.manufacturer.rsc[,2])
+
+column.standard.data <- tibble(type = "metadata",
+                               label = colnames(ct.manufacturer),
+                               PC1 = ct.manufacturer.csc[,1],
+                               PC2 = ct.manufacturer.csc[,2])
+
+plot.standard.data <- bind_rows(row.standard.data, column.standard.data)
+
+ggplot(plot.standard.data, aes(x = PC1, y = PC2, color = type, label = label)) +
+  geom_point() + xlim(-3,3) + ylim(-3,3) +
+  geom_vline(xintercept = 0, lty = "dashed", alpha = .5) +
+  geom_hline(yintercept = 0, lty = "dashed", alpha = .5) +
+  geom_label_repel(show.legend = F, segment.alpha = .5, point.padding = unit(5, "points")) +
+  theme_light() + coord_fixed()
+
+# principal coordinates
+row.principal.data <- tibble(type = "cluster",
+                            label = rownames(ct.manufacturer),
+                            PC1 = ct.manufacturer.rpc[,1],
+                            PC2 = ct.manufacturer.rpc[,2])
+
+column.principal.data <- tibble(type = "metadata",
+                               label = colnames(ct.manufacturer),
+                               PC1 = ct.manufacturer.cpc[,1],
+                               PC2 = ct.manufacturer.cpc[,2])
+
+plot.principal.data <- bind_rows(row.principal.data, column.principal.data)
+
+ggplot(plot.principal.data, aes(x = PC1, y = PC2, color = type, label = label)) +
+  geom_point() + xlim(-1,2) + ylim(-1,1) +
+  geom_vline(xintercept = 0, lty = "dashed", alpha = .5) +
+  geom_hline(yintercept = 0, lty = "dashed", alpha = .5) +
+  geom_label_repel(show.legend = F, segment.alpha = .5, point.padding = unit(5, "points")) +
+  theme_light() + coord_fixed()
+
+# scale 1: row principal
+
+plot.row.principal <- bind_rows(row.principal.data, column.standard.data)
+
+ggplot(plot.row.principal, aes(x = PC1, y = PC2, color = type, label = label)) +
+  geom_point() + xlim(-3,3) + ylim(-3,3) +
+  geom_vline(xintercept = 0, lty = "dashed", alpha = .5) +
+  geom_hline(yintercept = 0, lty = "dashed", alpha = .5) +
+  geom_label_repel(show.legend = F, segment.alpha = .5, point.padding = unit(5, "points")) +
+  theme_light() + coord_fixed()
